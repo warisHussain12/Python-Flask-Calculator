5284698d26b3f45f90a8bca7b66b00b0db0c1235
Waris Hussain <onlyhectic@gmail.com>
2025-02-10 14:27:58 +0530

    Update app.py
    added 2 lines in the end

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
@@ -13,4 +15,6 @@ def hello_world():
     return render_template('index.html')
     
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
